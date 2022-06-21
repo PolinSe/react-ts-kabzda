f3927830ab2741bbe8c25028476fb86909d87277
Polina Sedunova <lina.malina.spb@gmail.com>
2022-06-21 20:40:47 +0300

    feat: add some components

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string,
+    collapsed: boolean
+}
+
+function Accordion(props: AccordionPropsType) {
+    return (
+        <div>
+            <AccordionTitle title={ props.titleValue }/>
+            <AccordionBody collapsed={ props.collapsed }/>
+        </div>
+    );
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return <h3>{ props.title }</h3>
+}
+
+type AccordionBodyPropsType = {
+    collapsed: boolean
+}
+
+function AccordionBody(props:AccordionBodyPropsType) {
+    if (props.collapsed) {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+    }
+    return <></>
+
+}
+
+export default Accordion;
