b42f444edb9df4afa2a496d21a89ad5ebd88f775
Polina Sedunova <lina.malina.spb@gmail.com>
2022-11-12 23:38:50 +0300

    refactor: Rating storybook

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -6,12 +6,13 @@ export default {
   component: Rating,
 }
 
-export const EmptyRating = () => <Rating value={0} onClick={x=>x}/>;
-export const Rating1 = () => <Rating value={1} onClick={x=>x}/>;
-export const Rating2 = () => <Rating value={2} onClick={x=>x}/>;
-export const Rating3 = () => <Rating value={3} onClick={x=>x}/>;
-export const Rating4 = () => <Rating value={4} onClick={x=>x}/>;
-export const Rating5 = () => <Rating value={5} onClick={x=>x}/>;
+export const EmptyRating = () => <Rating value={0}/>;
+export const Rating1 = () => <Rating value={1}/>;
+export const Rating2 = () => <Rating value={2}/>;
+export const Rating3 = () => <Rating value={3}/>;
+export const Rating4 = () => <Rating value={4}/>;
+export const Rating5 = () => <Rating value={5}/>;
+
 export const RatingChanging = () => {
   const [rating, setRating] = useState<RatingValueType>(1);
   return <Rating value={rating} onClick={setRating}/>
